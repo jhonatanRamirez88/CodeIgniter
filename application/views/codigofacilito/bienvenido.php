1726611502f6d410fdd43452bb45b1913ca16580
punki03 <tonny_jho@MacBook-Pro-de-Jhonatan.local>
2016-04-15 22:24:03 -0500

    agregado la aplicación del code

---
+++ application/views/codigofacilito/bienvenido.php
@@ -0,0 +1,7 @@
+
+<body>
+<h1>Llamado desde el controlador Código facilito</h1>
+
+
+</body>
+</html>
---
+++ application/views/codigofacilito/headers.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>CodeIgniter</title>	
+</head>
