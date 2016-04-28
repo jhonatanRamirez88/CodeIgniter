3d91ca0f335063b71eba8960a454427383ab75fc
punki03 <tonny_jho@MacBook-Pro-de-Jhonatan.local>
2016-04-28 10:47:40 -0500

    llenos los datos del doctor y mostrando la hora fin y de inicio de consultas del doctor

--- application/views/cita/vista_2.php
+++ application/views/cita/vista_2.php
@@ -1,21 +1,41 @@
+<head> <h1>Cita de pasciente</h1></head>
 <section>
 
 	<form method="POST" action="<?php echo base_url(); ?>index.php/Cita/buscar">						
 			<div class="row uniform 50%">
 				<div class="4u 12u$(xsmall)">
-				Selecciona el día
-					<div class="select-wrapper">
-						<select id="horario" >
-						<option value=""></option>
-						<?php foreach ($horas as $doc): ?>
-							<option class="valor" value="<?php echo $doc['cve']?>"><?php echo $doc['descripcion']?></option>
-						<?php endforeach; ?>			
-						</select>
-					</div><!-- termina el div select wrapper-->
-				</div>
-
-
+					Nombre del doctor:					
+					<?php foreach ($horas as $doc): ?>
+						<label name="nombre" value="<?php echo $doc['cdoc']?>"><?php echo $doc['nom']?> <?php echo $doc['appat']?></label>
+					<?php endforeach; ?>					
+				</div>		
 			</div>
+
+
+			<div class="row uniform 50%">
+				<div class="4u 12u$(xsmall)">
+					Hora inicio de consultas:					
+					<?php foreach ($horas as $doc): ?>
+						<label name="hini" value=""><?php echo $doc['hini']?></label>
+					<?php endforeach; ?>					
+				</div>		
+			</div>
+
+			<div class="row uniform 50%">
+				<div class="4u 12u$(xsmall)">
+					Hora fin de consultas:					
+					<?php foreach ($horas as $doc): ?>
+						<label name="hfin" value=""><?php echo $doc['hfin']?></label>
+					<?php endforeach; ?>					
+				</div>		
+			</div>
+
+				
+	<div class="row uniform 50%">
+		<div class="4u 12u$(xsmall)">
+			Hora que desea la consulta:<input name="hrcon" type="text"></input>
+		</div>
+	</div>
 
 
 
