c47a55628b48ee9df65efa5e2c6d68f6fac9d23e
punki03 <tonny_jho@hotmail.com>
2016-06-02 19:55:31 -0500

    ultimas

--- application/views/bienvenido.php
+++ application/views/bienvenido.php
@@ -2,9 +2,4 @@
 			<section id="banner">
 				<h2>Bienvenido</h2>
 				<p></p>
-				<ul class="actions">
-					<li>
-						<a href="#" class="button big">Learn More</a>
-					</li>
-				</ul>
 			</section>
--- application/views/cr_esp.php
+++ application/views/cr_esp.php
@@ -1,6 +1,6 @@
 			<section>
 				<h1><?php echo $title; ?></h1>		
-<form action="../alta" method="post">
+<form action="<?php echo base_url(); ?>index.php/Especialidad/recibirdatos" method="post">
 	<div class="row uniform 50%">
  		<div class="7u 12u$(xsmall)">
 			<input type="text" name="descripcion" value="" placeholder="Nombre" />
